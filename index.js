0839e05e2ffacd0596f42cfc914793c4749bf1b0
Luana <santos.luanabeatriz1995@gmail.com>
2023-07-05 22:36:56 +0100

    Criação do middleware de autenticação da documentação

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ const logger = require('morgan');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerOptions = { customCssUrl: '/swagger-ui.css' };
-
 const routes = require('./src/routes');
-
+const authDocProducao = require('./src/middlewares/authDoc');
 const app = express();
 require('dotenv').config();
 
@@ -24,8 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 if(process.env.NODE_ENV !== 'test'){
     const swaggerFile = require('./swagger/swagger_output.json');
-    app.get('/', (req, res) => {/* #swagger.ignore = true */ res.redirect('/doc'); });
-    app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
+    app.get('/', (req, res) => { /* #swagger.ignore = true */ res.redirect('/doc'); });
+    app.use('/doc', authDocProducao, swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
 }
 
 
@@ -34,7 +33,7 @@ routes(app);
 
 
 
-if(process.env.NODE_ENV !== 'test'){
+if (process.env.NODE_ENV !== 'test') {
     const PORT = process.env.PORT || 4000;
     app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 }
