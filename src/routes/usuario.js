676872882cd7a201b828e5a681911cbb2e6ab193
Luana <santos.luanabeatriz1995@gmail.com>
2023-07-12 11:53:37 +0100

    Criada nova rota de login

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -1,27 +1,63 @@
 const express = require("express");
 const conectarBancoDados = require("../middlewares/conectarDB");
 const tratarErrosEsperados = require("../functions/tratarErrosEsperados");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const EsquemaUsuario = require("../models/usuario");
 const router = express.Router();
 
 router.post("/criar", conectarBancoDados, async function (req, res) {
-  try{
+  try {
     // #swagger.tags = ['Usuario']
-    let {nome, email, senha} = req.body;
+    let { nome, email, senha } = req.body;
     const numeroVezesHash = 10;
     const senhaHash = await bcrypt.hash(senha, numeroVezesHash);
-    const respostaBD = await EsquemaUsuario.create({nome, email, senha: senhaHash});
+    const respostaBD = await EsquemaUsuario.create({
+      nome,
+      email,
+      senha: senhaHash,
+    });
 
     res.status(200).json({
-      status:"OK",
+      status: "OK",
       statusMensagem: "Usuário criado com sucesso",
-      resposta: respostaBD
+      resposta: respostaBD,
     });
-  }catch(error){
-    if(String(error).includes("email_1 dup key")){
-      return tratarErrosEsperados(res, "Error: Já existe uma conta com este e-mail.");
+  } catch (error) {
+    if (String(error).includes("email_1 dup key")) {
+      return tratarErrosEsperados(
+        res,
+        "Error: Já existe uma conta com este e-mail."
+      );
     }
+    return tratarErrosEsperados(res, error);
+  }
+});
+
+router.post("/logar", conectarBancoDados, async function (req, res) {
+  try {
+    // #swagger.tags = ['Usuario']
+    let { email, senha } = req.body;
+    let respostaBD = await EsquemaUsuario.findOne({ email }).select("+senha");
+    if (respostaBD) {
+      let senhaCorreta = await bcrypt.compare(senha, respostaBD.senha);
+      if (senhaCorreta) {
+        let token = jwt.sign({ id: respostaBD._id }, process.env.JWT_SECRET, {
+          expiresIn: "1d",
+        });
+        res.header("x-auth-token", token);
+        res.status(200).json({
+          status: "OK",
+          statusMensagem: "Usuário autenticado com sucesso",
+          resposta: { "x-auth-token": token },
+        });
+      } else {
+        throw new Error("E-mail ou senha incorreta");
+      }
+    } else {
+      throw new Error("E-mail ou senha incorreta");
+    }
+  } catch (error) {
     return tratarErrosEsperados(res, error);
   }
 });
