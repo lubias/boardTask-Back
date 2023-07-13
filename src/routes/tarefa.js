2a0c742e99166df017ae3f3bc151cfdb750ce962
Luana <santos.luanabeatriz1995@gmail.com>
2023-07-13 22:54:20 +0100

    Criação da rota para obter todas as tarefas  do usuario logado

--- src/routes/tarefa.js
+++ src/routes/tarefa.js
@@ -1,51 +1,88 @@
-const express = require('express');
-const tratarErrosEsperados = require('../functions/tratarErrosEsperados');
-const authUser = require('../middlewares/authUser');
-const EsquemaTarefa = require('../models/tarefa');
-const conectarBancoDados = require('../middlewares/conectarDB');
+const express = require("express");
+const tratarErrosEsperados = require("../functions/tratarErrosEsperados");
+const authUser = require("../middlewares/authUser");
+const EsquemaTarefa = require("../models/tarefa");
+const conectarBancoDados = require("../middlewares/conectarDB");
 const router = express.Router();
 
-
-router.post('/criar', authUser, conectarBancoDados, async function (req, res) {
+router.post("/criar", authUser, conectarBancoDados, async function (req, res) {
   try {
     // #swagger.tags = ['Tarefa']
     let { posicao, titulo, descricao, status, dataEntrega } = req.body;
     const usuarioCriador = req.usuarioJwt.id;
-    const respostaBD = await EsquemaTarefa.create({ posicao, titulo, descricao, status, dataEntrega, usuarioCriador });
+    const respostaBD = await EsquemaTarefa.create({
+      posicao,
+      titulo,
+      descricao,
+      status,
+      dataEntrega,
+      usuarioCriador,
+    });
 
     res.status(200).json({
       status: "OK",
       statusMensagem: "Tarefa criada com sucesso.",
-      resposta: respostaBD
-    })
-
+      resposta: respostaBD,
+    });
   } catch (error) {
     return tratarErrosEsperados(res, error);
   }
 });
 
-router.put('/editar/:id', authUser, conectarBancoDados, async function (req, res) {
+router.put(
+  "/editar/:id",
+  authUser,
+  conectarBancoDados,
+  async function (req, res) {
+    try {
+      // #swagger.tags = ['Tarefa']
+      let idTarefa = req.params.id;
+      let { posicao, titulo, descricao, status, dataEntrega } = req.body;
+      const usuarioLogado = req.usuarioJwt.id;
+
+      const checkTarefa = await EsquemaTarefa.findOne({
+        _id: idTarefa,
+        usuarioCriador: usuarioLogado,
+      });
+      if (!checkTarefa) {
+        throw new Error("Tarefa não encontrada ou pertence a outro usuário");
+      }
+
+      const tarefaAtualizada = await EsquemaTarefa.updateOne(
+        { _id: idTarefa },
+        { posicao, titulo, descricao, status, dataEntrega }
+      );
+      if (tarefaAtualizada?.modifiedCount > 0) {
+        const dadosTarefa = await EsquemaTarefa.findOne({
+          _id: idTarefa,
+        }).populate("usuarioCriador");
+
+        res.status(200).json({
+          status: "OK",
+          statusMensagem: "Tarefa atualizada com sucesso.",
+          resposta: dadosTarefa,
+        });
+      }
+    } catch (error) {
+      return tratarErrosEsperados(res, error);
+    }
+  }
+);
+
+router.get("/obter/usuario", authUser, conectarBancoDados, async function (req, res) {
   try {
     // #swagger.tags = ['Tarefa']
-    let idTarefa = req.params.id;
-    let { posicao, titulo, descricao, status, dataEntrega } = req.body;
+    // #swagger.description = "Endpoint para obter todas as tarefas do usuario logado"
     const usuarioLogado = req.usuarioJwt.id;
+    const respostaBD = await EsquemaTarefa.find({
+      usuarioCriador: usuarioLogado
+    });
 
-    const checkTarefa = await EsquemaTarefa.findOne({ _id: idTarefa, usuarioCriador: usuarioLogado });
-    if (!checkTarefa) {
-      throw new Error("Tarefa não encontrada ou pertence a outro usuário");
-    }
-
-    const tarefaAtualizada = await EsquemaTarefa.updateOne({ _id: idTarefa }, { posicao, titulo, descricao, status, dataEntrega });
-    if (tarefaAtualizada?.modifiedCount > 0) {
-      const dadosTarefa = await EsquemaTarefa.findOne({ _id: idTarefa }).populate('usuarioCriador');
-
-      res.status(200).json({
-        status: "OK",
-        statusMensagem: "Tarefa atualizada com sucesso.",
-        resposta: dadosTarefa
-      })
-    }
+    res.status(200).json({
+      status: "OK",
+      statusMensagem: "Tarefas listadas na resposta com sucesso.",
+      resposta: respostaBD,
+    });
   } catch (error) {
     return tratarErrosEsperados(res, error);
   }
