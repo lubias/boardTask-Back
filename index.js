fbf505416f45c3cd47ae07272ceee741f798e22b
Luana <santos.luanabeatriz1995@gmail.com>
2023-07-16 21:23:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 if(process.env.NODE_ENV !== 'test'){
     const swaggerFile = require('./swagger/swagger_output.json');
     app.get('/', (req, res) => { /* #swagger.ignore = true */ res.redirect('/doc'); });
-    app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
+    app.use('/doc', /*authDocProducao,*/ swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
 }
 
 
